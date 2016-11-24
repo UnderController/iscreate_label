9fcfd5523016818ff06d4ecfef69a81b58e44c4d
chenzomi12 <chenzomi12@gmail.com>
2016-11-24 15:15:10 +0800

    Deleted files manually

--- test.py
+++
@@ -1,8 +0,0 @@
-from core.framedata import get_label_data
-from core.imgpprocess import SaveImage, PreProcess
-import cv2
-
-
-if __name__ == '__main__':
-    img = cv2.imread("image-00505.jpeg")
-    PreProcess(img)
